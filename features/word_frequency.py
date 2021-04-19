d019f9ab08df60d0ac86cd9a1fe5d925f69b6837
AimaZubar <Aimakh@users.noreply.github.com>
2021-04-19 22:26:07 +0500

    feature

--- features/word_frequency.py
+++ features/word_frequency.py
@@ -19,3 +19,26 @@ class WordFrequency():
 
         for i, doc in enumerate(X):
             segments = []
+            for entry in doc:
+                class_sum = 0
+                word_count = 0
+                uncommon = 0
+                entry = entry.lower()
+                for w in word_tokenize(entry):
+                    w = re.sub('[^a-zA-Z]+', '', w)
+                    if not w: continue
+
+                    word_count+=1
+                    word_class = self.word_class.get(w, 20)
+                    if word_class == 20:
+                        uncommon += 1
+                    class_sum += word_class
+
+                segments.append([class_sum/word_count, uncommon/word_count])
+
+            transformed.append(segments)
+            print_progress_bar(i + 1, len(X), description = 'word frequency')
+
+        feature_names.extend(['average_word_class', 'uncommon_words'])
+
+        return transformed
